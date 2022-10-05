dee6ac2a2a3d020d66e5054607e52b47001b147a
Andrew Cornejo <atcornejo3@gmail.com>
2022-10-04 20:03:45 -0700

    committing on dev branch

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://yjeotjawmpwujgsnitzu.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqZW90amF3bXB3dWpnc25pdHp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5MDYwNTIsImV4cCI6MTk4MDQ4MjA1Mn0.YAu6ZKUPlwOEDFNm0pLCKZU0b19WbM6WkI-TQeWwAsk';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
@@ -27,3 +28,26 @@ export async function signOutUser() {
 }
 
 /* Data functions */
+export async function uploadImage(bucketName, imagePath, imageFile) {
+    const bucket = client.storage.from(bucketName);
+
+    const response = await bucket.upload(imagePath, imageFile, {
+        cacheControl: '3600',
+        // in this case, we will _replace_ any
+        // existing file with same name.
+        upsert: true,
+    });
+
+    if (response.error) {
+        // eslint-disable-next-line no-console
+        console.log(response.error);
+        return null;
+    }
+
+    // Construct the URL to this image:
+    const url = `${SUPABASE_URL}/storage/v1/object/public/${response.data.Key}`;
+    // URL Looks like:
+    // https://nwxkvnsiwauieanvbiri.supabase.co/storage/v1/object/public/images/pets/984829079656/Franky.jpeg
+
+    return url;
+}
